7ebb5250e804342dab29c9d49bddced949f9f5aa
Tran Gia Huy <hanoivis2@gmail.com>
2020-06-03 08:44:37 +0700

    no message

---
+++ SchoolManagement/src/SchoolManagement/SchoolManagement.java
@@ -0,0 +1,11 @@
+package SchoolManagement;
+
+public class SchoolManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First commit");
+	}
+
+}
